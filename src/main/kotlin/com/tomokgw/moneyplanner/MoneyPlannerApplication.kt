05c18c889ed40fdf513e3a9135df1c3c3b7c559a
tkagawa <kagawa@sumasate.jp>
2024-12-30 19:52:42 +0900

    Create

---
+++ src/main/kotlin/com/tomokgw/moneyplanner/MoneyPlannerApplication.kt
@@ -0,0 +1,11 @@
+package com.tomokgw.moneyplanner
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MoneyPlannerApplication
+
+fun main(args: Array<String>) {
+    runApplication<MoneyPlannerApplication>(*args)
+}
